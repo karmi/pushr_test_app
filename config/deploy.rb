72e2f50224ccb80f73464b11f7e5e024f243597c
Karel Minarik <karmi@karmi.cz>
2008-11-11 23:53:42 +0100

    Deploy.rb cleanup & adding a "hard" restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,8 +19,16 @@ role :web, "karmi.cz"
 role :db,  "karmi.cz", :primary => true
 
 # = Hooks
-task :after_update_code, :roles => [:app, :db] do
-  # link shared files
+after "deploy:update_code" do
   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   run "ln -nfs #{shared_path}/production.sqlite3 #{release_path}/db/production.sqlite3"
+end
+
+# = Tasks
+# Let's do a "hard" restart (reaper really doesn't restart Mongrel...)
+namespace :deploy do
+  task :restart do
+    stop
+    start
+  end
 end
